b116c0e832d978fbeb64c56510c47c2aade25ada
Cherry <54757449+cherryeury@users.noreply.github.com>
2022-02-23 13:48:36 +0700

    fixes

--- npc/xcustom/System/itemInfos.lua
+++ npc/xcustom/System/itemInfos.lua
@@ -1,56 +1,92 @@
-----------------------------------------------------------
+	----------------------------------------------------------
 ------ rathena
 ------ sader1992
 ------ https://rathena.org/board/profile/30766-sader1992/
 ----------------------------------------------------------
-function add_url()
-end
-function add_item_link(v)
-end
 function main()
-	IInfo = {"System.import_iteminfo","System.itemInfo_true_V5"}
-	-- Example!
-	-- IInfo = {"System.import_iteminfo","System.kro_iteminfo5","System.kro_iteminfo4","System.kro_iteminfo3","System.kro_iteminfo2","System.kro_iteminfo1","System.kro_iteminfo"}
-	ItemList = {}
-	for key,ItemInfo in pairs(IInfo) do
-		require(ItemInfo)
-		for ItemID, DESCS in pairs(tbl) do
-			if not ItemList[ItemID] then
-				ItemList[ItemID] = true
-				result, msg = AddItem(ItemID, DESCS.unidentifiedDisplayName, DESCS.unidentifiedResourceName, DESCS.identifiedDisplayName, DESCS.identifiedResourceName, DESCS.slotCount, DESCS.ClassNum)
-				if not result == true then
-					return false, msg
-				end
-				AddItemUnidentifiedDesc(ItemID, add_url())
-				for k, v in pairs(DESCS.unidentifiedDescriptionName) do
-					result, msg = AddItemUnidentifiedDesc(ItemID, v)
-					if not result == true then
-						return false, msg
-					end
-				end
-				AddItemUnidentifiedDesc(ItemID, add_item_link(ItemID))
-				AddItemIdentifiedDesc(ItemID, add_url())
-				for k, v in pairs(DESCS.identifiedDescriptionName) do
-					result, msg = AddItemIdentifiedDesc(ItemID, v)
-					if not result == true then
-						return false, msg
-					end
-				end
-				AddItemIdentifiedDesc(ItemID, add_item_link(ItemID))
-				if nil ~= DESCS.EffectID then
-					result, msg = AddItemEffectInfo(ItemID, DESCS.EffectID)
-				end
-				if not result == true then
-					return false, msg
-				end
-				if nil ~= DESCS.costume then
-					result, msg = AddItemIsCostume(ItemID, DESCS.costume)
-				end
+	a = {}
+	e = false
+	local functions = require("System.function")
+	require("System.import_iteminfo")	-- THE Import file name here !!
+	for ItemID, DESCS in pairs(tbl) do
+		result, msg = AddItem(ItemID, DESCS.unidentifiedDisplayName, DESCS.unidentifiedResourceName, DESCS.identifiedDisplayName, DESCS.identifiedResourceName, DESCS.slotCount, DESCS.ClassNum)
+		if not result == true then
+			return false, msg
+		end
+		AddItemUnidentifiedDesc(ItemID, functions.add_custom())
+		for k, v in pairs(DESCS.unidentifiedDescriptionName) do
+			result, msg = AddItemUnidentifiedDesc(ItemID, v)
+			if not result == true then
+				return false, msg
+			end
+		end
+		AddItemUnidentifiedDesc(ItemID, functions.add_arena_url(ItemID))
+		AddItemIdentifiedDesc(ItemID, functions.add_custom())
+		for k, v in pairs(DESCS.identifiedDescriptionName) do
+			result, msg = AddItemIdentifiedDesc(ItemID, v)
+			if not result == true then
+				return false, msg
+			end
+		end
+		AddItemIdentifiedDesc(ItemID, functions.add_arena_url(ItemID))
+		if nil ~= DESCS.EffectID then
+			result, msg = AddItemEffectInfo(ItemID, DESCS.EffectID)
+		end
+		if not result == true then
+			return false, msg
+		end
+		if nil ~= DESCS.costume then
+			result, msg = AddItemIsCostume(ItemID, DESCS.costume)
+		end
+		if not result == true then
+			return false, msg
+		end
+		a[#a+1] = ItemID
+	end
+	require("System.itemInfo_EN")	-- The kro iteminfo file name here !!!
+	for ItemID, DESC in pairs(tbl) do
+		print(tostring(ItemID))
+		for _,v in pairs(a) do
+			if v == ItemID then
+					e = true
+				break
+			end
+		end
+		if not e == true then
+			result, msg = AddItem(ItemID, DESC.unidentifiedDisplayName, DESC.unidentifiedResourceName, DESC.identifiedDisplayName, DESC.identifiedResourceName, DESC.slotCount, DESC.ClassNum)
+			if not result == true then
+				return false, msg
+			end
+			AddItemUnidentifiedDesc(ItemID, functions.add_clear())
+			for k, v in pairs(DESC.unidentifiedDescriptionName) do
+				result, msg = AddItemUnidentifiedDesc(ItemID, v)
 				if not result == true then
 					return false, msg
 				end
 			end
+			AddItemUnidentifiedDesc(ItemID, functions.add_url(ItemID))
+			AddItemIdentifiedDesc(ItemID, functions.add_clear())
+			for k, v in pairs(DESC.identifiedDescriptionName) do
+				result, msg = AddItemIdentifiedDesc(ItemID, v)
+				if not result == true then
+					return false, msg
+				end
+			end
+			AddItemIdentifiedDesc(ItemID, functions.add_url(ItemID))
+			if nil ~= DESC.EffectID then
+				result, msg = AddItemEffectInfo(ItemID, DESC.EffectID)
+			end
+			if not result == true then
+				return false, msg
+			end
+			if nil ~= DESC.costume then
+				result, msg = AddItemIsCostume(ItemID, DESC.costume)
+			end
+			if not result == true then
+				return false, msg
+			end
 		end
+		e = false
 	end
 	return true, "good"
 end
